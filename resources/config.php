4f9da2cbac935adf4c373fa239817503fab8d145
Manish Kannan <manishkannan125@gmail.com>
2020-06-13 10:10:53 -0700

    fixing db

--- resources/config.php
+++ resources/config.php
@@ -11,15 +11,15 @@ defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "temp
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
 
 defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY", __DIR__ . DS . "uploads");
-//$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//defined("DB_HOST") ? null : define("DB_HOST", $cleardb_url['host']);
-//defined("DB_USER") ? null : define("DB_USER", $cleardb_url['user']);
-//defined("DB_PASS") ? null : define("DB_PASS", $cleardb_url['pass']);
-//defined("DB_NAME") ? null : define("DB_NAME", substr($cleardb_url["path"], 1));
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+defined("DB_HOST") ? null : define("DB_HOST", $cleardb_url['host']);
+defined("DB_USER") ? null : define("DB_USER", $cleardb_url['user']);
+defined("DB_PASS") ? null : define("DB_PASS", $cleardb_url['pass']);
+defined("DB_NAME") ? null : define("DB_NAME", substr($cleardb_url["path"], 1));
 
 
-//$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-$connection = mysqli_connect('localhost', 'root', '', 'ecom_db');
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//$connection = mysqli_connect('localhost', 'root', '', 'ecom_db');
 require_once("functions.php");
 require_once("cart.php");
 
